9f2a5b41ae4be8dd33493941bb32da30e2355c43
Vipretha <89765930+Vipretha@users.noreply.github.com>
2021-09-13 22:49:29 -0700

    Add files via upload

---
+++ list.py
@@ -0,0 +1,31 @@
+values=["a","b","c","d","e","f"]
+print("the values of : ",values)
+
+value1=["a","b","c","d","e","f"]
+value1[1]=1
+print("the values of : ",value1)
+
+value2=["a","b","c","d","e","f"]
+value2[1:3]=["1","2"]
+print("the values of : ",value2)
+value3=["a","b","c","d","e","f"]
+value3.insert(2,"watermelon")
+print("the values of : ",value3)
+value4=["a","b","c","d","e","f"]
+value4.append(1)
+print("the values of : ",value4)
+value6=["a","b","c","d","e","f"]
+value6.pop(1)
+print("the values of : ",value6)
+value7=["a","b","c","d","e","f"]
+value7.pop()
+print("the values of : ",value7)
+value8=["a","b","c","d","e","f"]
+del values[2]
+print("the values of : ",value8)
+value9=["a","b","c","d","e","f"]
+del values[1]
+print("the values of : ",value9)
+
+
+
