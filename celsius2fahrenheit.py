097a8bfa9f27d43862175f44bf90ff5339934548
Vipretha <89765930+Vipretha@users.noreply.github.com>
2021-09-27 21:52:58 -0700

    Add files via upload

---
+++ celsius2fahrenheit.py
@@ -0,0 +1,34 @@
+celsius=float(input("enter the celsius"))
+fahrenheit=(celsius * 1.8)+32
+print("The value of fahrenheit",fahrenheit)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
---
+++ swapping.py
@@ -0,0 +1,7 @@
+x=2
+y=3
+z=x
+x=y
+y=z
+print("the swapping value of x",x)
+print("the swapping value of y",y)
