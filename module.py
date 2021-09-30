4dc652523d473e419928143662789f048b948e52
Vipretha <89765930+Vipretha@users.noreply.github.com>
2021-09-29 21:42:33 -0700

    Add files via upload

---
+++ module.py
@@ -0,0 +1,9 @@
+import functionmarkdetails
+name=input("enter the name :") 	
+dpt=input("enter the dpt :") 
+M1=input("enter the M1 :")
+M2=input("enter the M2 :")
+M3=input("enter the M3 :")
+M4=input("enter the M4 :")
+M5=input("enter the M5 :")
+functionmarkdetails.cal(name,dpt,M1,M2,M3,M4,M5)
